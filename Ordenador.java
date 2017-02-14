fa7b36e0fb6202ee8a721cb26634e73cffc8d911
Israel0123 <isrm_123@hotmail.com>
2017-02-14 14:42:53 -0600

    Ordenador
    Metodo en el cual se ordenan los numeros del vector.

---
+++ Ordenador.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inserccion;
+
+/**
+ *
+ * @author isrm_
+ */
+public class Ordenador {
+    
+    
+    public static void OrdenarInserccion(int[] array)
+    {
+        int aux;
+        int cont1;
+        int cont2;
+        
+        for(cont1=1; cont1<array.length; cont1++)
+        {
+            aux=array[cont1];
+            for(cont2=cont1-1; cont2>=0 && array[cont2]>aux; cont2--)
+            {
+                array[cont2+1]=array[cont2];
+                array[cont2]=aux;
+            }
+        }
+    }
+    
+}
